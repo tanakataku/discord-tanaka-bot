58f0302d326078976ac7b31225ea433a772dff9f
BURI <85824872+tanakataku@users.noreply.github.com>
2022-03-25 17:48:31 +0900

    test

---
+++ src/commands/help.js
@@ -0,0 +1,11 @@
+module.exports={
+    data:{
+        name:"help",
+        description:"TANAKAのhelpを表示します"
+    },
+    run:{
+        tanaka(discord,data){
+            discord.i.reply(data.map(command=>`commandname:${command.name},description:${command.description}`).join("/n"))
+        }
+    }
+}
---
+++ src/main_tanaka.js
@@ -0,0 +1,23 @@
+const { Client, Intents } = require('discord.js');
+const fs = require("node:fs");
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES] });
+const commands = {};
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const data = [];
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  commands[command.data.name] = command.run;
+  data.push(commands.data);
+};
+client.on("ready", async () => {
+  client.application.commands.set(data, 'サーバーID');
+});
+client.on("interactionCreate", interaction => {
+  if (!interaction.isCommand()) return;
+  try {
+    commands[interaction.commandName].tanaka({c:client,i:interaction},data);
+  } catch (e) {
+    console.log(e);
+  };
+});
+client.login(process.env.token);
