747f484f85c1f51c75341f350b7f5e8264cece9c
BURI <85824872+tanakataku@users.noreply.github.com>
2022-03-25 17:58:39 +0900

    修正

--- src/main_tanaka.js
+++ src/main_tanaka.js
@@ -2,7 +2,7 @@ const { Client, Intents } = require('discord.js');
 const fs = require("node:fs");
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_PRESENCES] });
 const commands = {};
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync("./src/commands").filter(file => file.endsWith('.js'));
 const data = [];
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
@@ -10,7 +10,7 @@ for (const file of commandFiles) {
   data.push(commands.data);
 };
 client.on("ready", async () => {
-  client.application.commands.set(data, 'サーバーID');
+  client.application.commands.set(data, '956839228317265980');
 });
 client.on("interactionCreate", interaction => {
   if (!interaction.isCommand()) return;
